c01dec1697dbdfa06debcee8e01e60cb724fbc9f
Olga <boa906680@gmail.com>
2021-08-14 10:46:44 +0300

    add main section
    add main section

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const sass = require('gulp-sass')(require('sass'));
+var sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
@@ -13,13 +13,14 @@ gulp.task('server', function() {
         }
     });
 
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(rename({ suffix: '.min', prefix: '' }))
-        .pipe(autoprefixer({ overrideBrowserslist: ['last 15 versions'], grid: true }))
+        .pipe(autoprefixer())
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
@@ -27,7 +28,6 @@ gulp.task('styles', function() {
 
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-});
+})
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
