5cce1a0c11e31dc572ec43e41af4349557fddade
Olga <boa906680@gmail.com>
2021-08-15 08:24:37 +0300

    Add second section and slider
    Add second section and slider

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right.png"></button>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
+    });
+});
